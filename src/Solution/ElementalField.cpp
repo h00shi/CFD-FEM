dcbb3d86858ee33af0527d62cb8d697cc0109af6
rabbit <nburgess3@gmail.com>
2015-10-24 18:24:27 -0400

    Added update to elemental field.

--- src/Solution/ElementalField.cpp
+++ src/Solution/ElementalField.cpp
@@ -12,11 +12,6 @@ ElementalField::ElementalField(const UnstMesh& mesh,
                                const Array1D<intT> p,
                                const Array1D<intT> nvar)
 {
-  intT field_size = 0;
-  for(intT i = 0; i < mesh.get_MeshElements().get_nelement(); i++){
-    field_size +=
-
-  }
 
 
 }
